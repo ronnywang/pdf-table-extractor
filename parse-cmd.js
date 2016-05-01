fc6a5c793e9cb11de9e110b097a6798c2c9d5123
Ronny Wang <ronnywang@gmail.com>
2016-05-01 21:39:43 +0800

    add fake Image object && cmap

--- parse-cmd.js
+++ parse-cmd.js
@@ -1,6 +1,7 @@
 // Ex: node parse-cmd.js samples/pta_10229_131308_94274.pdf
 
 var fs = require('fs');
+Image = function(){ };
 
 // HACK few hacks to let PDF.js be loaded not as a module in global space.
 require('./pdf.js/examples/node/domstubs.js');
@@ -8,6 +9,9 @@ require('./pdf-table-extractor.js');
 
 // Run `gulp dist` to generate 'pdfjs-dist' npm package files.
 PDFJS = require('./pdf.js/build/dist');
+PDFJS.cMapUrl = './pdf.js/build/generic/web/cmaps/';
+PDFJS.cMapPacked = true;
+
 
 // Loading file from file system into typed array
 var pdfPath = process.argv[2];
